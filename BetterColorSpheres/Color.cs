51f19505a3d8cb846717d5c76f678968b7c29649
Drackout <mikatx2@hotmail.com>
2025-04-03 18:50:02 +0100

    put vars with get public for access

--- BetterColorSpheres/Color.cs
+++ BetterColorSpheres/Color.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 namespace BetterColorSpheres
 {
     public class Color
-    {
-        
+    {        
+        public byte Red {get;}
+        public byte Green {get;}
+        public byte Blue {get;}
+        public byte Alpha {get;}
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+
+        public Color(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = 255;
+        }
+
+
+
+        public string GetColor
+        {
+            get => $"{Red},{Green},{Blue},{Alpha}";
+        }
+
+        public byte GetGray
+        {
+            get => (byte)((Red+Green+Blue)/3);
+        }
+
     }
 }
--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -6,7 +6,15 @@ namespace BetterColorSpheres
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Color cc1 = new Color(225,29,111,255);
+
+            Console.WriteLine(cc1.Red);
+            Console.WriteLine(cc1.Green);
+            Console.WriteLine(cc1.Blue);
+            Console.WriteLine(cc1.Alpha);
+            Console.WriteLine(cc1.GetColor);
+            Console.WriteLine(cc1.GetGray);
+
         }
     }
 }
