c52a378f787c96a141466eb1de33b9ea490d445d
Drackout <mikatx2@hotmail.com>
2025-04-04 14:14:49 +0100

    create and add multiple proprieties

---
+++ MyRPG/Player.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace MyRPG
+{
+    public class Player
+    {
+        private int xp;
+        private float health;
+
+        public string Name {get;set;}
+
+        public int XP
+        {
+            get => xp;
+            set 
+            {
+                if (value > 0)
+                    xp += value;
+            }
+        }
+
+        public int Level => (1+XP/1000);
+
+        public float Health => 100+(Level-1)*20;
+
+        public void TakeDamage(float damage)
+        {
+            health -= damage;
+            if (health > 0)
+                health = 0;
+
+            XP += (int)damage / 20;
+        }
+
+        public Player(string name)
+        {
+            
+        }
+
+
+    }
+}
